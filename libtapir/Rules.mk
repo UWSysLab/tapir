a9a320ac83f0f17d08b23a682f1b16d8d703db92
Irene Zhang <iyzhang@zookeeper.cs.washington.edu>
2016-02-10 10:43:12 -0800

    adding some additional files to libtapir

--- libtapir/Rules.mk
+++ libtapir/Rules.mk
@@ -1,6 +1,6 @@
 d := $(dir $(lastword $(MAKEFILE_LIST)))
 
-$(d)libtapir.so: $(patsubst %.o,%-pic.o, $(OBJS-tapir-client))
+$(d)libtapir.so: $(patsubst %.o,%-pic.o, $(LIB-message) $(LIB-store-common) $(LIB-store-frontend) $(LIB-udptransport) $(OBJS-tapir-client))
 LDFLAGS-$(d)libtapir.so += -shared
 
 BINS += $(d)libtapir.so
